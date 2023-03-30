07184871d8da6c76345730cc00f51ea0386d2aa5
udonggi <dongki1882@naver.com>
2023-03-30 15:10:54 +0900

    아이디, 비밀번호 유효성 체크

--- src/main/java/com/ll/gramgram/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/ll/gramgram/boundedContext/member/controller/MemberController.java
@@ -6,6 +6,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @Controller
 @RequestMapping("/member")
@@ -16,8 +21,19 @@ public class MemberController {
         return "usr/member/join";
     }
 
+    @AllArgsConstructor
+    @Getter
+    public static class JoinForm {
+        @NotBlank
+        @Size(min = 4, max = 30)
+        private final String username;
+        @NotBlank
+        @Size(min = 4, max = 30)
+        private final String password;
+    }
+
     @PostMapping("/join")
-    public String join() {
+    public String join(@Valid JoinForm joinForm) {
         return "redirect:/";
     }
 }
