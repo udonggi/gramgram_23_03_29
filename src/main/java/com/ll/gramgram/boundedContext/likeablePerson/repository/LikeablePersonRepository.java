ff98e056ea0c5243ba80ea4d9efda83a6e7a5955
udonggi <dongki1882@naver.com>
2023-04-04 13:44:11 +0900

    호감표시하기

---
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/repository/LikeablePersonRepository.java
@@ -0,0 +1,8 @@
+package com.ll.gramgram.boundedContext.likeablePerson.repository;
+
+import com.ll.gramgram.boundedContext.likeablePerson.entity.LikeablePerson;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikeablePersonRepository extends JpaRepository<LikeablePerson, Integer> {
+
+}
